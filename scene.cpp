932ac0e8649218af35262ef86c928ff1474fb0d8
MizutaniKanata <mizutani_kanata@yahoo.co.jp>
2026-05-27 16:34:17 +0900

    Id 変更

--- scene.cpp
+++ scene.cpp
@@ -11,117 +11,117 @@
 #include "define.h"
 using namespace DirectX;
 
-static SCENE scene = SCENE_TITLE;
+static SCENE scene = SCENE_DEBUG_SCORE;
 
-void Init(void)
+void Init( void )
 {
-	switch (scene)
-	{	
-	case SCENE_TITLE:
+	switch ( scene )
+	{
+		case SCENE_TITLE:
 		Title_Initialize();
 		break;
-	case SCENE_GAME:
+		case SCENE_GAME:
 		Game_Initialize();
 		break;
-	case SCENE_RESULT:
+		case SCENE_RESULT:
 		Result_Initialize();
-		break;	
-	case SCENE_DEBUG_MODEL:
+		break;
+		case SCENE_DEBUG_MODEL:
 		DebugModelScene_Initialize();
 		break;
-	case SCENE_DEBUG_LIGHTING:
+		case SCENE_DEBUG_LIGHTING:
 		DebugLightingScene_Initialize();
 		break;
-	case SCENE_DEBUG_SCORE:
+		case SCENE_DEBUG_SCORE:
 		Debugscore_Initialize();
 		break;
-	default:
+		default:
 		break;
 	}
 }
 
-void Update(void)
+void Update( void )
 {
-	switch (scene)
+	switch ( scene )
 	{
-	case SCENE_TITLE:
+		case SCENE_TITLE:
 		Title_Update();
 		break;
-	case SCENE_GAME:
+		case SCENE_GAME:
 		Game_Update();
 		break;
-	case SCENE_RESULT:
+		case SCENE_RESULT:
 		Result_Update();
 		break;
-	case SCENE_DEBUG_MODEL:
+		case SCENE_DEBUG_MODEL:
 		DebugModelScene_Update();
 		break;
-	case SCENE_DEBUG_LIGHTING:
+		case SCENE_DEBUG_LIGHTING:
 		DebugLightingScene_Update();
 		break;
-	case SCENE_DEBUG_SCORE:
+		case SCENE_DEBUG_SCORE:
 		Debugscore_Update();
 		break;
-	default:
+		default:
 		break;
 	}
 }
 
-void Draw(void)
+void Draw( void )
 {
-	switch (scene)
+	switch ( scene )
 	{
-	case SCENE_TITLE:
+		case SCENE_TITLE:
 		Title_Draw();
 		break;
-	case SCENE_GAME:
+		case SCENE_GAME:
 		Game_Draw();
 		break;
-	case SCENE_RESULT:
+		case SCENE_RESULT:
 		Result_Draw();
 		break;
-	case SCENE_DEBUG_MODEL:
+		case SCENE_DEBUG_MODEL:
 		DebugModelScene_Draw();
 		break;
-	case SCENE_DEBUG_LIGHTING:
+		case SCENE_DEBUG_LIGHTING:
 		DebugLightingScene_Draw();
 		break;
-	case SCENE_DEBUG_SCORE:
+		case SCENE_DEBUG_SCORE:
 		Debugscore_Draw();
 		break;
-	default:
+		default:
 		break;
 	}
 }
 
-void Finalize(void)
+void Finalize( void )
 {
-	switch (scene)
+	switch ( scene )
 	{
-	case SCENE_TITLE:
+		case SCENE_TITLE:
 		Title_Finalize();
 		break;
-	case SCENE_GAME:
+		case SCENE_GAME:
 		Game_Finalize();
 		break;
-	case SCENE_RESULT:
+		case SCENE_RESULT:
 		Result_Finalize();
 		break;
-	case SCENE_DEBUG_MODEL:
+		case SCENE_DEBUG_MODEL:
 		DebugModelScene_Finalize();
 		break;
-	case SCENE_DEBUG_LIGHTING:
+		case SCENE_DEBUG_LIGHTING:
 		DebugLightingScene_Finalize();
 		break;
-	case SCENE_DEBUG_SCORE:
+		case SCENE_DEBUG_SCORE:
 		Debugscore_Finalize();
 		break;
-	default:
+		default:
 		break;
 	}
 }
 
-void SetScene(SCENE id)
+void SetScene( SCENE id )
 {
 	Finalize();
 
@@ -130,7 +130,7 @@ void SetScene(SCENE id)
 	Init();
 }
 
-SCENE GetScene(void)
+SCENE GetScene( void )
 {
 	return scene;
 }
